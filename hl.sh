6eb7d8d7cc3d0af628b5b39e7dfb5ca577b2d30e
snown <snown27@gmail.com>
2012-08-17 08:43:16 -0700

    Adjusting script to handle empty args better

--- hl.sh
+++ hl.sh
@@ -3,7 +3,7 @@
 absPath () {
     myPath=$1
     
-    fileType=`stat -f "%HT" "$1"`
+    fileType=`stat -f "%HT" "$1"` || { exit 1; }
     if [[ "$fileType" == "Symbolic Link" ]]; then
         myPath=`readlink "$1"`
     fi
@@ -11,13 +11,13 @@ absPath () {
     echo `perl -MCwd=realpath -e "print realpath '$myPath'"`
 }
 
-SOURCE_DIR=`absPath "$1"`
-LINK_DIR=`dirname "$SOURCE_DIR"`
+# SOURCE_DIR=`absPath "$1"`
+# LINK_DIR=`dirname "$SOURCE_DIR"`
 
 # argCount=$#
 # echo $argCount
 # 
-# echo
+echo #> ~/hlLog
 
 i=1
 for arg in "$@"; do
@@ -25,19 +25,23 @@ for arg in "$@"; do
     
     fileList[$i-1]=$ARG_PATH
     
-    # echo "Arg $i: $arg --> $ARG_PATH"
+    echo "Arg $i: $arg --> $ARG_PATH" #>> ~/hlLog
     
     let "i++"
 done
 
-# echo
-# 
-# echo $fileList
-# 
-# echo
+echo
 
-# echo "rsync -aLP --link-dest=\"$LINK_DIR\" \"${fileList[@]}\"" # > ~/hlLog
+LINK_DIR=`dirname "${fileList[0]}"`
 
-# echo >> ~/hlLog
+echo #>> ~/hlLog
 
-rsync -aLP --link-dest="$LINK_DIR" "${fileList[@]}" # >> ~/hlLog
+echo "Link Directory = $LINK_DIR" #>> ~/hlLog
+
+echo #>> ~/hlLog
+
+echo "rsync -aLP --link-dest=\"$LINK_DIR\" \"${fileList[@]}\"" #>> ~/hlLog
+
+echo #>> ~/hlLog
+
+rsync -aLP --link-dest="$LINK_DIR" "${fileList[@]}" #>> ~/hlLog
